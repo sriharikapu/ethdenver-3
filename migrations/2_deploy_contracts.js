41eaac91ecd85f8cf104a143f1608635b11b5610
Memo Ramirez <mramirez@vsapartners.com>
2018-02-18 00:10:23 -0700

    Increased start and end time

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ var YumCoinCrowdSale = artifacts.require("./YumCoinCrowdSale.sol");
 var Web3 = web3;
 
 module.exports = function (deployer, network, accounts) {
-  const startTime = Web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
-  const endTime = startTime // 20 days
+  const startTime = Web3.eth.getBlock(web3.eth.blockNumber).timestamp + 10 // 10 second in the future
+  const endTime = Web3.eth.getBlock(web3.eth.blockNumber).timestamp + 15 // 15 seconds in the future
   const rate = new Web3.BigNumber(1000)
   const wallet = accounts[0]
 
